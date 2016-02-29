d605913b4a9b1472e595f8278b6ddaac0e759bde
Dave Bush <github@david-bush.co.uk>
2016-02-29 09:57:51 +0000

    Added instance variable getter to Cuba Helpers.

--- cuba_helpers.rb
+++ cuba_helpers.rb
@@ -1,4 +1,10 @@
 module CubaHelpers
+
+  def get_attr(attr)
+    globals = self.instance_variable_get("@g")
+    globals[attr]
+  end
+
   def raise404()
       res.status = 404
       res.write view("404")
