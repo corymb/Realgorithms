830d08636c7dec5baae885f829ee30febabfb2f1
Dave Bush <github@david-bush.co.uk>
2016-02-29 10:52:04 +0000

    Added to_json method to cuba helpers.

--- cuba_helpers.rb
+++ cuba_helpers.rb
@@ -1,5 +1,11 @@
 module CubaHelpers
 
+  def to_json(data)
+    """Returns data as a JSON response """
+      res.headers["Content-Type"] = "application/json; charset=utf-8"
+      res.write data.to_json
+  end
+
   def get_attr(attr)
     globals = self.instance_variable_get("@g")
     globals[attr]
