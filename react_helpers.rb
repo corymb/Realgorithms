bdfdcba13323e01a08af796fdd745e87aa122011
Dave Bush <github@david-bush.co.uk>
2016-02-29 10:52:31 +0000

    Removed react_loader from class.

--- react_helpers.rb
+++ react_helpers.rb
@@ -1,9 +1,2 @@
 module ReactHelpers
-  react_loader = "if (typeof(Opal) !== 'undefined') {" \
-      'Opal.mark_as_loaded("opal");' \
-      'Opal.mark_as_loaded("corelib/runtime.self");' \
-      'Opal.mark_as_loaded("jquery.self");' \
-      'Opal.mark_as_loaded("sources/react.self");' \
-      'Opal.load("reacting");' \
-  "}"
 end
