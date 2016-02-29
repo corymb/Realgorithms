10c5588de2964de91a998fe41c1a5734aec45245
Dave Bush <github@david-bush.co.uk>
2016-02-29 10:50:48 +0000

    Added insertion sort and file read method.

--- algorithms.rb
+++ algorithms.rb
@@ -1,12 +1,17 @@
 module Helpers
+  def read_file(filename)
+    File.read(filename)
+  end
+
   def get_algorithm(type)
     algorithms = {
       :mergesort => 'Test mergesort code',
-      :quicksort => 'Test quicksort code'
+      :quicksort => 'Test quicksort code',
+      :insertionsort => 'insertion_sort.py'
     }
     if not algorithms[type]
       raise404()
     end
-    algorithms[type]
+    read_file algorithms[type]
   end
 end
